960b4901b834a24fe94aff85da818ae68965bb50
Beyrem Weslati <74115035+beyremweslati@users.noreply.github.com>
2023-06-23 18:03:42 +0100

    Delete Calculating probabilities.py

--- Introduction to Statistics in Python/Calculating probabilities.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-amir_deals = pd.read_csv("amir_deals.csv")
-# Count the deals for each product
-counts = amir_deals['product'].value_counts()
-
-# Calculate probability of picking a deal with each product
-probs = counts / amir_deals.shape[0]
-print(probs)
